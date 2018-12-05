3648404759b3639b0b0f06f1c2fe54224e548ce8
jonsin92 <jon.barrenechea2017@twk.cl>
2018-12-05 13:15:54 -0300

    1.0.4

--- config/database.php
+++ config/database.php
@@ -56,6 +56,22 @@ return [
             'engine' => null,
         ],
 
+        'local' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST_LOCAL', '127.0.0.1'),
+            'port' => env('DB_PORT_LOCAL', '3306'),
+            'database' => env('DB_DATABASE_LOCAL', 'forge'),
+            'username' => env('DB_USERNAME_LOCAL', 'forge'),
+            'password' => env('DB_PASSWORD_LOCAL', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
